7ffb675db3b98fff8a519215abf1896bc0ef744d
faysaljafry <faysaljafryofficial@gmail.com>
2021-10-07 20:49:24 +0500

    ng Forms, Get data in js object through ngForms etc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  @ViewChild('f') signUpForm!: NgForm;
+  suggestUsername() {
+    const suggestName = 'SuperUser';
+  }
+
+  onSubmit() {
+    console.log(this.signUpForm);
+  }
   title = 'ng-forms';
 }
